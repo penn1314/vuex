6e0f9b0c2811041b7edf29d492c26789cfee8eea
penn1314 <1198749011@qq.com>
2022-03-08 20:13:52 +0800

    first commit

---
+++ src/components/Addition.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="add-container">
+    <h3>当前最新的count值为：</h3>
+    <button>+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Addition',
+}
+</script>
+
+<style></style>
